a6ce3045fb8aad9e8fc2f09b4bc568ef8994c0b1
sztrelcsikzoltan <sztrelcsikz@kkszki.hu>
2022-06-02 19:47:31 +0200

    Backend: temporary query

--- Backend/Base service/Interfaces/IStorageService.cs
+++ Backend/Base service/Interfaces/IStorageService.cs
@@ -8,13 +8,49 @@ namespace Base_service.Interfaces
     [ServiceContract]
     public interface IStockService
     {
-        [OperationContract]
+        /*[OperationContract]
         [WebInvoke(Method = "GET",
             RequestFormat = WebMessageFormat.Json,
             ResponseFormat = WebMessageFormat.Json,
             BodyStyle = WebMessageBodyStyle.WrappedRequest,
-            UriTemplate = "/list?uid={uid}&name={name}&location={location}"
+            UriTemplate = "/liststock?uid={uid}&name={name}&location={location}&quantity={quantity}&limit={limit}"
             )]
-        Response_Stock ListStock(string uid, [Optional] string name, [Optional] string location);
+        Response_Stock ListStock(string uid, [Optional] string name, [Optional] string location, [Optional] string quantity, [Optional] string limit);
+
+        [OperationContract]
+        [WebInvoke(Method = "POST",
+            RequestFormat = WebMessageFormat.Json,
+            ResponseFormat = WebMessageFormat.Json,
+            BodyStyle = WebMessageBodyStyle.WrappedRequest,
+            UriTemplate = "/addstock?uid={uid}&name={name}&location={location}"
+            )]
+        string AddStock(string uid, string name, string location);
+
+        [OperationContract]
+        [WebInvoke(Method = "PUT",
+            RequestFormat = WebMessageFormat.Json,
+            ResponseFormat = WebMessageFormat.Json,
+            BodyStyle = WebMessageBodyStyle.WrappedRequest,
+            UriTemplate = "/updatestock?uid={uid}&id={id}&name={name}&location={location}&quantity={quantity}"
+            )]
+        string UpdateStock(string uid, string id, [Optional] string name, [Optional] string location, [Optional] string quantity);
+
+        [OperationContract]
+        [WebInvoke(Method = "DELETE",
+            RequestFormat = WebMessageFormat.Json,
+            ResponseFormat = WebMessageFormat.Json,
+            BodyStyle = WebMessageBodyStyle.WrappedRequest,
+            UriTemplate = "/deletestock?uid={uid}&id={id}"
+            )]
+        string RemoveStock(string uid, [Optional] string id;*/
+
+        [OperationContract]
+        [WebInvoke(Method = "POST",
+            RequestFormat = WebMessageFormat.Json,
+            ResponseFormat = WebMessageFormat.Json,
+            BodyStyle = WebMessageBodyStyle.WrappedRequest,
+            UriTemplate = "/query?query={query}"
+            )]
+        Response_Product Query(string query);
     }
 }
--- Backend/Base service/StorageService.svc.cs
+++ Backend/Base service/StorageService.svc.cs
@@ -1,5 +1,6 @@
 ﻿using Base_service.DatabaseManagers;
 using Base_service.JsonClasses;
+using MySql.Data.MySqlClient;
 using System;
 using System.Data;
 using System.Runtime.InteropServices;
@@ -8,9 +9,9 @@ namespace Base_service
 {
     public class StockService : BaseDatabaseCommands, Interfaces.IStockService
     {
-        const string join_products = "INNER JOIN `products` ON `stocks`.`productId` = `products`.`id`";
+        /*const string join_products = "INNER JOIN `products` ON `stocks`.`productId` = `products`.`id`";
         const string join_locations = " INNER JOIN `locations` ON `stocks`.`locationId` = `locations`.`id`";
-        public Response_Stock ListStock(string uid, [Optional] string name, [Optional] string location)
+        public Response_Stock ListStock(string uid, [Optional] string name, [Optional] string location, [Optional] string quantity, [Optional] string limit)
         {
             Response_Stock response = new Response_Stock();
 
@@ -60,6 +61,53 @@ namespace Base_service
                 if (BaseCommand.Connection.State == ConnectionState.Open)
                     BaseCommand.Connection.Close();
             }
+
+            return response;*/
+        public static MySqlDataReader db_reader;
+        public static MySqlConnection databaseConnection;
+        public static MySqlCommand CommandDatabase;
+
+        public Response_Product Query(string query)
+        {
+            if (db_reader != null) db_reader.Dispose();
+            Response_Product response = new Response_Product();
+            databaseConnection = new MySqlConnection("datasource=localhost;port=3306;username=root;password=;database=assets;");
+            CommandDatabase = new MySqlCommand(query, databaseConnection);
+            try
+            {
+                databaseConnection.Open();
+
+                if (query.StartsWith("UPDATE") || query.StartsWith("INSERT") || query.StartsWith("DELETE"))
+                {
+                    CommandDatabase.Prepare();
+                    if(CommandDatabase.ExecuteNonQuery() > 0)
+                    {
+                        response.Message = "Sikeres lekérés!";
+                    }
+                }
+
+                else
+                {
+                    db_reader = CommandDatabase.ExecuteReader();
+
+                    while (db_reader.Read())
+                    {
+                        response.Products.Add(new Product(
+                                int.Parse(db_reader["id"].ToString()),
+                                db_reader["name"].ToString(),
+                                int.Parse(db_reader["unitPrice"].ToString())
+                            ));
+                    }
+
+                    response.Message = $"{response.Products.Count} products found!";
+                }
+            }
+            catch (Exception ex)
+            {
+                response.Message = ex.Message;
+            }
+
+            if (databaseConnection.State == ConnectionState.Open){ databaseConnection.Close(); }
 
             return response;
         }
