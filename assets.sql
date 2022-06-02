3295be9c07b519fac6ad1c8753865fb41f852966
sztrelcsikzoltan <sztrelcsikz@kkszki.hu>
2022-06-02 20:19:40 +0200

    Backend: updating assets.sql

--- assets.sql
+++ assets.sql
@@ -44,7 +44,7 @@ CREATE TABLE `purchases` (
   `id` int(12) PRIMARY KEY AUTO_INCREMENT,
   `productId` int(12),
   `quantity` int(12),
-  `date` date,
+  `date` datetime,
   `locationId` int(12),
   `userId` int(12),
   FOREIGN KEY (`userId`) REFERENCES `users`(`id`),
@@ -56,13 +56,21 @@ CREATE TRIGGER `tr_purchase_after`
 AFTER INSERT ON `purchases` FOR EACH ROW 
 UPDATE `stocks` SET `stocks`.`quantity` = `stocks`.`quantity` + NEW.`quantity` 
 WHERE `stocks`.`locationId` = NEW.`locationId` 
-AND `stocks`.`productId` = NEW.`locationId`;
+AND `stocks`.`productId` = NEW.`productId`;
+
+CREATE TRIGGER `tr_purchase_before` 
+BEFORE UPDATE ON `purchases` FOR EACH ROW 
+UPDATE `stocks` SET `stocks`.`quantity` = `stocks`.`quantity` + (NEW.`quantity` - (
+SELECT `quantity` FROM `purchases` 
+WHERE `id` = NEW.`id`))
+WHERE `stocks`.`locationId` = NEW.`locationId` 
+AND `stocks`.`productId` = NEW.`productId`;
 
 CREATE TABLE `sales` (
   `id` int(12) PRIMARY KEY AUTO_INCREMENT,
   `productId` int(12),
   `quantity` int(12),
-  `date` date,
+  `date` datetime,
   `locationId` int(12),
   `userId` int(12),
   FOREIGN KEY (`userId`) REFERENCES `users`(`id`),
@@ -73,5 +81,13 @@ CREATE TABLE `sales` (
 CREATE TRIGGER `tr_sold_after` 
 AFTER INSERT ON `sales` FOR EACH ROW 
 UPDATE `stocks` SET `stocks`.`quantity` = `stocks`.`quantity` - NEW.`quantity` 
+WHERE `stocks`.`locationId` = NEW.`locationId` 
+AND `stocks`.`productId` = NEW.`productId`;
+
+CREATE TRIGGER `tr_sold_before` 
+BEFORE UPDATE ON `sales` FOR EACH ROW 
+UPDATE `stocks` SET `stocks`.`quantity` = `stocks`.`quantity` - (NEW.`quantity` - (
+SELECT `quantity` FROM `sales` 
+WHERE `id` = NEW.`id`))
 WHERE `stocks`.`locationId` = NEW.`locationId` 
 AND `stocks`.`productId` = NEW.`productId`;
