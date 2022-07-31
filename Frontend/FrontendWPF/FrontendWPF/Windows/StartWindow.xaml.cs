7def23245cf9e0bd265407ab0ce7a61ec6dfc748
sztrelcsikzoltan <sztrelcsikz@kkszki.hu>
2022-07-31 21:43:39 +0200

    Remove unused usings in StartWindow.xaml.cs

--- Frontend/FrontendWPF/FrontendWPF/Windows/StartWindow.xaml.cs
+++ Frontend/FrontendWPF/FrontendWPF/Windows/StartWindow.xaml.cs
@@ -1,21 +1,17 @@
-﻿using System;
+﻿using FrontendWPF.Classes;
+using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 using System.Windows.Threading;
-using FrontendWPF.Classes;
-using Microsoft.Win32;
 
 namespace FrontendWPF.Windows
 {
